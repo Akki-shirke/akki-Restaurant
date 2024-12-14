24abca91f924718be2acc69fd023dc614752c1a4
Akki-shirke <shirkeanket42@gmail.com>
2024-12-14 19:01:40 +0530

    first commit

---
+++ RS/script.js
@@ -0,0 +1,16 @@
+(document).ready(function () {
+
+    $("a[href^='#']").on("click", function (event) {
+
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top,
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+
+    });
+});
